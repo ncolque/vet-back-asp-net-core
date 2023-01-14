28d364bcb22dea12057d848c1034b1afebd4259e
Nicolas Colque <nicolascon93@gmail.com>
2023-01-14 15:54:20 -0400

    Creacion del modelo, migracion, controlador y conexion a la bd

--- VetBackASPNETCore/Program.cs
+++ VetBackASPNETCore/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using VetBackASPNETCore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add context
+builder.Services.AddDbContext<AplicationDbContext>(optionsAction =>
+{
+    optionsAction.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"));
+});
 
 var app = builder.Build();
 
