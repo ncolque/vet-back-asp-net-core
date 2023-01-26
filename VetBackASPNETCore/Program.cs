ed82adeb38dbf026077800faf4236a4ec055ac68
Nicolas Colque <nicolascon93@gmail.com>
2023-01-25 22:05:52 -0400

    Agregamos el servicio de los Cors

--- VetBackASPNETCore/Program.cs
+++ VetBackASPNETCore/Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Cors
+builder.Services.AddCors(options => options.AddPolicy("AllowWebapp", builder => builder
+.AllowAnyOrigin()
+.AllowAnyHeader()
+.AllowAnyMethod()));
+
 //Add context
 builder.Services.AddDbContext<AplicationDbContext>(optionsAction =>
 {
@@ -24,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowWebapp");
 
 app.UseHttpsRedirection();
 
